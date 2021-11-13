7b44055c3a671b9cef500ca8d6d74e3be6604588
mattpaulhus <93746218+mattpaulhus@users.noreply.github.com>
2021-11-13 15:22:00 -0800

    Update analytics Google Event.js

--- assets/js/analytics Google Event.js
+++ assets/js/analytics Google Event.js
@@ -1,10 +1,3 @@
-<script>
-/**
-* Function that registers a click on an outbound link in Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label. Setting the transport method to 'beacon' lets the hit be sent
-* using 'navigator.sendBeacon' in browser that support it.
-*/
 var getOutboundLink = function(url) {
   gtag('event', 'click', {
     'event_category': 'outbound',
@@ -13,4 +6,3 @@ var getOutboundLink = function(url) {
     'event_callback': function(){document.location = url;}
   });
 }
-</script>
