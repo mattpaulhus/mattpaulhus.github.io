3dcdd280c58540cb6bfa82822872be0b23c35e07
mattpaulhus <93746218+mattpaulhus@users.noreply.github.com>
2021-11-13 15:40:38 -0800

    Update analytics Google Event.js

--- assets/js/analytics Google Event.js
+++ assets/js/analytics Google Event.js
@@ -1,6 +1,6 @@
 var getOutboundLink = function(url) {
   gtag('event', 'click', {
-    'event_category': 'outbound',
+    'event_category': 'outbound to LinkedIn',
     'event_label': url,
     'transport_type': 'beacon',
     'event_callback': function(){document.location = url;}
